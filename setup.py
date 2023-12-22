27f26ec9d296c00b18017262357b701010383009
acrovato <39187559+acrovato@users.noreply.github.com>
2023-12-22 11:45:04 +0100

    Force Gmsh 4.10 as 4.11 is bugged for SU2 outputs

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ setup(
     version=__version__,
     description='GmshCFD is a tool based on Gmsh for creating CFD meshes around lifting surfaces suitable for external aerodynamic analysis.',
     keywords='Gmsh CFD aerodynamic mesh Python',
-    author='',
+    author='Adrien Crovato',
     author_email='',
     url='https://github.com/acrovato/gmshcfd',
     license='GNU General Public License 3.0',
     packages=find_packages(include=['gmshcfd*']),
-    install_requires=['numpy>=1.22', 'gmsh>=4.11'],
+    install_requires=['numpy>=1.22', 'gmsh==4.10.5'],
     classifiers=['Operating System :: OS Independent', 'Programming Language :: Python'],
 )
